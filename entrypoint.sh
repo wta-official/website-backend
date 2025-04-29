2b359767d65ec2248a4da6f9100abd9210a2d925
sarmhi <sammiejay813@gmail.com>
2025-04-29 18:08:05 +0100

    deployment: updated static file collection command in entrypointfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ echo "🔁 Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "📦 Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --verbosity 2
+
 
 echo "👤 Creating superuser (if not exists)..."
 python manage.py createsuperuser_with_defaults || true
