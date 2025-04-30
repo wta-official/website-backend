0cf549b065a52aba2ee0b8b3169faa346606779b
sarmhi <sammiejay813@gmail.com>
2025-04-30 08:23:56 +0100

    fix: added autofield for better id generation

--- api/models.py
+++ api/models.py
@@ -14,6 +14,7 @@ class JobType(models.TextChoices):
 
 # Abstract base model
 class TimestampedModel(models.Model):
+    id = models.AutoField(primary_key=True) 
     created_at = models.DateTimeField(default=now)
     updated_at = models.DateTimeField(auto_now=True)
 
