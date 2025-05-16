1a1240a6cb0a6421fc348f8789f64d6807244586
sarmhi <sammiejay813@gmail.com>
2025-05-16 10:27:05 +0100

    deployment: added database url in the settings for connection to the db

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
 
@@ -105,14 +105,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     # 'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("POSTGRES_DB"),
-        'USER': os.getenv("POSTGRES_USER"),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT': os.getenv("POSTGRES_PORT")
-    }
+    'default': dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        ),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv("POSTGRES_DB"),
+    #     'USER': os.getenv("POSTGRES_USER"),
+    #     'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+    #     'HOST': os.getenv("POSTGRES_HOST"),
+    #     'PORT': os.getenv("POSTGRES_PORT")
+    # }
 }
 
 
